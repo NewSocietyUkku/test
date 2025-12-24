e9f6432538a7b680661eceb3ee0ef1e71dcdfc18
NewSocietyUkku <Sandysalbego@protonmail.com>
2025-12-24 18:56:55 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'emotion-cards-v1';
+const urlsToCache = [
+  '/mockBiz/',
+  '/mockBiz/index.html',
+  '/mockBiz/manifest.json',
+  '/mockBiz/sw.js',
+  '/mockBiz/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
